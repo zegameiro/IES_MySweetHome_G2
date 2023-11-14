ac9a15d152e9adf8d1d5b404df3f2c21c831d006
d479 <daniel.madureira@ua.pt>
2023-11-14 15:54:48 +0000

    Navbar and routes initiated

---
+++ proj/MSH-frontend/src/components/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../layout/navbar/Navbar'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Dashboard
