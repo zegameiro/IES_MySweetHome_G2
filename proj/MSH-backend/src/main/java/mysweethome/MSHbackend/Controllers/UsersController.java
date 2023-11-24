ffeb9b157011b1a5330550782da2f93c6f2d9a6f
P-Ramos16 <p.ramos@ua.pt>
2023-11-24 19:55:54 +0000

    Fixed CORS for user endpoints

--- proj/MSH-backend/src/main/java/mysweethome/MSHbackend/Controllers/UsersController.java
+++ proj/MSH-backend/src/main/java/mysweethome/MSHbackend/Controllers/UsersController.java
@@ -2,19 +2,22 @@ package mysweethome.MSHbackend.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.server.ResponseStatusException;
 import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import mysweethome.MSHbackend.Models.*;
 import mysweethome.MSHbackend.Services.*;
 
-
+@Controller
+@CrossOrigin("*")
 @RestController
 @RequestMapping(path = "/user")
 public class UsersController {
--- proj/MSH-backend/src/main/java/mysweethome/MSHbackend/MshBackendApplication.java
+++ proj/MSH-backend/src/main/java/mysweethome/MSHbackend/MshBackendApplication.java
@@ -5,8 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -19,5 +24,17 @@ public class MshBackendApplication {
         SpringApplication.run(MshBackendApplication.class, args);
     }
 
+	//  Needed to bypass CORS errors on some browsers (such as Firefox)
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+		configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+		configuration.setAllowCredentials(false);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 
 }
