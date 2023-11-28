9e016d26e6846bb90d4920e8cb1e6c5eb16264e1
d479 <daniel.madureira@ua.pt>
2023-11-28 15:32:41 +0000

    Add page ui done

--- proj/MSH-frontend/src/components/pages/AddPage.jsx
+++ proj/MSH-frontend/src/components/pages/AddPage.jsx
@@ -13,24 +13,25 @@ const AddPage = () => {
       <Navbar />
       <div className="flex flex-col w-full h-full">
         <Header />
-
-        <div className="">
-          <button className="bg-primary rounded-xl shadow-lg p-4 m-4 flex justify-start items-end font-bold text-4xl w-full h-[27vh] m-full">
-            <h1>ADD ROOM</h1>
-          </button>
-        </div>
-        <div className="w-full h-full">
-          <button className="bg-primary rounded-xl shadow-lg p-4 m-4 flex justify-start items-end font-bold text-4xl w-full h-[27vh] m-full">
-            <h1>ADD DEVICE</h1>
-          </button>
-        </div>
-        <div className="w-full h-full">
-          <button
-            className="bg-primary rounded-xl shadow-lg p-4 m-4 flex justify-start items-end font-bold text-4xl w-full h-[27vh] m-full"
-            onClick={() => navigate('/')}
-          >
-            <h1>ADD ROUTINE</h1>
-          </button>
+        <div className="flex flex-row justify-between">
+          <div className='w-full h-full m-2'>
+            <button className="bg-[url('/src/assets/images/home1.jpg')] rounded-xl shadow-lg p-4 m-4 flex justify-start items-end font-bold text-3xl w-full h-[80vh] ">
+              <h1>ADD ROOM</h1>
+            </button>
+          </div>
+          <div className="w-full h-full m-2">
+            <button className="bg-[url('/src/assets/images/home2.jpg')] rounded-xl shadow-lg p-4 m-4 flex justify-start items-end font-bold text-3xl w-full h-[80vh] ">
+              <h1>ADD DEVICE</h1>
+            </button>
+          </div>
+          <div className="w-full h-full m-2">
+            <button
+              className="bg-[url('/src/assets/images/home3.jpg')] rounded-xl shadow-lg p-4 m-4 flex justify-start items-end font-bold text-3xl w-full h-[80vh] "
+              onClick={() => navigate('/')}
+            >
+              <h1>ADD ROUTINE</h1>
+            </button>
+          </div>
         </div>
       </div>
     </div>
