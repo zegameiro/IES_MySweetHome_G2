b4546c2deb855ef81b92fcf2db8a51753d9e3a4a
FiNeX96 <rodrigoaguiar96@ua.pt>
2023-12-11 20:49:14 +0000

    prod is working =)

--- proj/MSH-backend/src/main/java/mysweethome/MSHbackend/Controllers/RoutinesController.java
+++ proj/MSH-backend/src/main/java/mysweethome/MSHbackend/Controllers/RoutinesController.java
@@ -19,6 +19,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.media.Content;
+
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
