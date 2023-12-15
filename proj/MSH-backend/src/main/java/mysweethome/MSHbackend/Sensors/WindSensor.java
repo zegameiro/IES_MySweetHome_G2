3ca32a68805cd9aca3a5e351f2159643a7555069
FiNeX96 <rodrigoaguiar96@ua.pt>
2023-12-15 21:07:06 +0000

    fix wind sensor values

--- proj/MSH-backend/src/main/java/mysweethome/MSHbackend/Sensors/WindSensor.java
+++ proj/MSH-backend/src/main/java/mysweethome/MSHbackend/Sensors/WindSensor.java
@@ -15,11 +15,6 @@ public class WindSensor {
     Random RANDOM = new Random();
     String uniqueID = UUID.randomUUID().toString();
 
-    private static final double MIN_WIND_SPEED = 0.1; // Minimum wind speed in m/s
-    private static final double MAX_WIND_SPEED = 20.0; // Maximum wind speed in m/s
-    private static final double MEAN_WIND_SPEED = 5.0; // Mean wind speed in m/s
-    private static final double STANDARD_DEVIATION = 3.0; // Standard deviation for the normal distribution
-
     public WindSensor(String name, Channel queue, String queue_name, String device_category) {
         this.broker_queue = queue;
         this.queue_name = queue_name;
@@ -47,16 +42,24 @@ public class WindSensor {
         }
     }
 
+    private static final double MIN_WIND_SPEED = 0.1; // Minimum wind speed in m/s
+    private static final double MAX_WIND_SPEED = 20.0; // Maximum wind speed in m/s
+    private static final double EXTREME_PROBABILITY = 0.05; // Probability of an extreme value
+
     // Simulate a realistic wind strength value in the range of 0.1 m/s to 20 m/s
     public double getRandomWindStrength() {
 
         Random random = new Random();
-        double windSpeed = MIN_WIND_SPEED + (MAX_WIND_SPEED - MIN_WIND_SPEED) * (random.nextGaussian() * STANDARD_DEVIATION + MEAN_WIND_SPEED);
-        
-        // Ensure that the generated wind speed is within the specified range
-        windSpeed = Math.max(MIN_WIND_SPEED, Math.min(MAX_WIND_SPEED, windSpeed));
 
-        return windSpeed;
+        // Determine whether to generate a normal or extreme value
+        if (random.nextDouble() < EXTREME_PROBABILITY) {
+            // 5% chance for an extreme value
+            return random.nextDouble() < 0.5 ? MIN_WIND_SPEED + 4.0 * random.nextDouble()
+                    : MAX_WIND_SPEED - 4.0 * random.nextDouble();
+        } else {
+            // 90% chance for a normal value between 5 and 10
+            return 5.0 + 5.0 * random.nextDouble();
+        }
 
     }
 
