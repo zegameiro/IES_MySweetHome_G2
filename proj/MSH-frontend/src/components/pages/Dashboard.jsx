9fd990f8a762e80e6b3c3b32366cc80d2c9b484b
zegameiro <jose.mcgameiro@ua.pt>
2023-12-17 01:25:32 +0000

    Removed unusless imports

--- proj/MSH-frontend/src/components/pages/Dashboard.jsx
+++ proj/MSH-frontend/src/components/pages/Dashboard.jsx
@@ -65,7 +65,6 @@ const Dashboard = () => {
       const res = await axios.get(`${BASE_API_URL}/alerts/list`, null);
       if (res.status === 200) {
         setAlerts(res.data);
-        console.log("Alerts ->", res.data);
       }
     } catch (error) {
       console.log(error);
@@ -97,7 +96,6 @@ const Dashboard = () => {
 
   useEffect(() => {
     if (selectedRoom) {
-      console.log("Selected Room ->", selectedRoom);
       const roomOutDevices = outputDevices.filter(
         (outdevice) => selectedRoom.devices.includes(outdevice.id)
       );
