545bf92c43afd40c3e59d5525615a837b9dab05a
zegameiro <jose.mcgameiro@ua.pt>
2023-12-17 00:55:35 +0000

    Added stuff

---
+++ proj/MSH-frontend/src/components/layout/RoutinesCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RoutineCard = ({ routine }) => {
+    return (    
+        <div>
+            <h1>Routine card</h1>
+        </div>
+    );
+};
+
+export default RoutineCard;
