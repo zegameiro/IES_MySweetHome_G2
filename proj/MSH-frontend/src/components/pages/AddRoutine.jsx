c3330a034d695b2cc78d46d50ec30b2738c59ed6
d479 <daniel.madureira@ua.pt>
2023-12-18 18:53:11 +0000

    Routines done

--- proj/MSH-frontend/src/components/pages/AddRoutine.jsx
+++ proj/MSH-frontend/src/components/pages/AddRoutine.jsx
@@ -37,9 +37,12 @@ const AddRoutine = () => {
   const [devices, setDevices] = useState(null);
   const [actions, setActions] = useState(null);
   const [action, setAction] = useState(null);
+  const [sensors, setSensors] = useState(null);
+  const [name, setName] = useState(null);
 
   const [channel, setChannel] = useState(null);
   const [music, setMusic] = useState(null);
+  const [time, setTime] = useState(null);
 
   const [color, setColor] = useState('#ffffff');
   const [slider, setSlider] = useState(50);
@@ -47,8 +50,10 @@ const AddRoutine = () => {
   const [temperature, setTemperature] = useState(20);
   const [humidity, setHumidity] = useState(25);
 
-  const [categories, setCategories] = useState(null);
   const [selectDevice, setSelectDevice] = useState(null);
+  const [selectSensor, setSelectSensor] = useState(null);
+  const [sensorValue, setSensorValue] = useState(null);
+  const [sensorRange, setSensorRange] = useState([]);
 
   const navigate = useNavigate();
 
@@ -82,15 +87,12 @@ const AddRoutine = () => {
     }
   };
 
-  const getCategories = async () => {
+  const getSensors = async () => {
     try {
-      const res = await axios.get(
-        `${BASE_API_URL}/outputs/listCategories`,
-        null
-      );
+      const res = await axios.get(`${BASE_API_URL}/sources/list`, null);
       if (res.status === 200) {
-        setCategories(res.data);
-        console.log('cats -> ', res.data);
+        setSensors(res.data);
+        console.log('sensors -> ', res.data);
       }
     } catch (error) {
       console.log(error);
@@ -100,7 +102,7 @@ const AddRoutine = () => {
   useEffect(() => {
     if (user) {
       getDevices();
-      getCategories();
+      getSensors();
     } else {
       navigate('/login?redirect=dashboard');
     }
@@ -115,14 +117,127 @@ const AddRoutine = () => {
 
   const per_temperature = temperature * 1.6 + 16;
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submit');
+
+    console.log('trigger -> ', trigger);
+    console.log('action -> ', action);
+    console.log('selectDevice -> ', selectDevice);
+    console.log('name -> ', name);
+    console.log('time -> ', time);
+    console.log('sensorValue -> ', sensorValue);
+    console.log('selectSensor -> ', selectSensor);
+    console.log('channel -> ', channel);
+    console.log('music -> ', music);
+    console.log('color -> ', color);
+    console.log('slider -> ', slider);
+    console.log('temperature -> ', temperature);
+    console.log('humidity -> ', humidity);
+
+    // Check if routine changes an output device
+    let value = null;
+
+    if (action == 'Adjust Brightness ' || action == 'Adjust Volume') {
+      value = slider;
+    }
+    if (action == 'Adjust Temperature') {
+      value = temperature;
+    }
+    if (action == 'Adjust Humidity level') {
+      value = humidity;
+    }
+    if (action == 'Change Color') {
+      value = color;
+    }
+    if (action == 'Change Channel') {
+      value = channel;
+    }
+    if (action == 'Change Music') {
+      value = music;
+    }
+
+    // Check if routine is triggered by a timestamp
+    if (trigger && action && selectDevice && name) {
+      if (trigger == 'Time' && time) {
+        axios
+          .post(`${BASE_API_URL}/routines/addTB`, {
+            trigger_timestamp: time,
+            routine_name: name,
+            associated_action: {
+              action_title: action + ' ' + selectDevice.name,
+              action_description: action,
+              outputDeviceID: selectDevice.id,
+              action_newValue: value,
+            },
+          })
+          .then((res) => {
+            console.log(res);
+            navigate('/routines');
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+
+      // Check if routine is triggered by a range of values
+      if (trigger == 'Interval' && sensorRange) {
+        axios
+          .post(`${BASE_API_URL}/routines/addSB`, {
+            trigger_type: 'range',
+            input_ranges: sensorRange,
+            source_id: selectSensor,
+            routine_name: name,
+            associated_action: {
+              action_title: action + ' - ' + selectDevice.name,
+              action_description: action,
+              outputDeviceID: selectDevice.id,
+              action_newValue: value,
+            },
+          })
+          .then((res) => {
+            console.log(res);
+            navigate('/dashboard');
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+
+      // Check if routine is triggered by a specific value
+      if (trigger == 'Event' && sensorValue) {
+        axios
+          .post(`${BASE_API_URL}/routines/addSB`, {
+            trigger_type: 'exact',
+            exact_value: sensorValue,
+            source_id: selectSensor,
+            routine_name: name,
+            associated_action: {
+              action_title: action + ' - ' + selectDevice.name,
+              action_description: action,
+              outputDeviceID: selectDevice.id,
+              action_newValue: value,
+            },
+          })
+          .then((res) => {
+            console.log(res);
+            navigate('/dashboard');
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    }
+  };
+
   return (
     <div className="mx-[5%] mt-4 flex justify-between">
       <Navbar />
-      <div className="flex flex-col w-full h-full">
+      <div className="flex flex-col w-full h-full overflow-hidden">
         <Header />
-        <div className="w-full mx-4">
+        <form className="w-full mx-4">
           <h1 className="m-4 text-4xl font-bold">Add new routine</h1>
-          <div className="flex flex-col justify-between h-full w-[90%] ">
+          <div className="flex flex-col justify-between h-full w-[90%]">
             <h2 className="m-4 text-3xl text-slate-500">
               Select a known device
             </h2>
@@ -138,6 +253,7 @@ const AddRoutine = () => {
                     setSelectDevice(device);
                     setAction(null);
                   }}
+                  required
                 >
                   <OutputDeviceCard
                     device={device}
@@ -162,6 +278,7 @@ const AddRoutine = () => {
                         : 'badge-outline badge-accent'
                     } badge-xl `}
                     onClick={() => setAction(act)}
+                    required
                   >
                     <h1>{act}</h1>
                   </div>
@@ -202,6 +319,7 @@ const AddRoutine = () => {
                             min="0"
                             max="100"
                             value={slider}
+                            required
                           />
                           <BsBrightnessHighFill className="ml-2 text-4xl text-slate-500" />
                         </div>
@@ -220,6 +338,7 @@ const AddRoutine = () => {
                           style={{
                             width: '50%',
                           }}
+                          required
                         />
                         <div className="flex flex-col items-center justify-center w-1/4 p-4 text-2xl font-medium">
                           <h1>Selected color</h1>
@@ -274,6 +393,7 @@ const AddRoutine = () => {
                             }`}
                             onChange={(e) => setTemperature(e.target.value)}
                             value={temperature}
+                            required
                           />
                           <FaFire />
                         </div>
@@ -301,6 +421,7 @@ const AddRoutine = () => {
                             min="0"
                             max="100"
                             value={slider}
+                            required
                           />
                           <BsVolumeUpFill className="ml-2 text-4xl text-slate-500" />
                         </div>
@@ -349,6 +470,7 @@ const AddRoutine = () => {
                             }`}
                             onChange={(e) => setHumidity(e.target.value)}
                             value={humidity}
+                            required
                           />
                           <GiDroplets />
                         </div>
@@ -363,6 +485,7 @@ const AddRoutine = () => {
                           type="text"
                           placeholder="Enter a music"
                           onChange={(e) => setMusic(e.target.value)}
+                          required
                         />
                       </div>
                     );
@@ -370,7 +493,10 @@ const AddRoutine = () => {
                   case 'Change Channel':
                     return (
                       <div className="flex flex-col items-center justify-center w-full p-4 text-2xl shadow-lg bg-base-100 rounded-xl">
-                        <select className="w-full text-2xl select select-bordered select-primary">
+                        <select
+                          className="w-full text-2xl select select-bordered select-primary"
+                          required
+                        >
                           <option
                             disabled
                             selected
@@ -397,83 +523,131 @@ const AddRoutine = () => {
               })()}
             </div>
 
-            <h2 className="m-4 text-3xl text-slate-500">Select Trigger</h2>
-            <span className="w-full divider" />
-            <div className="flex flex-row justify-around w-full m-4 overflow-auto">
-              <span
-                className={`flex flex-col items-center justify-center p-4 m-4 text-2xl badge ${
-                  trigger == 'Time'
-                    ? 'badge-primary'
-                    : 'badge-outline badge-accent'
-                } badge-xl `}
-                onClick={() => setTrigger('Time')}
-              >
-                Time
-              </span>
-              <span
-                className={`flex flex-col items-center justify-center p-4 m-4 text-2xl badge ${
-                  trigger == 'Interval'
-                    ? 'badge-primary'
-                    : 'badge-outline badge-accent'
-                } badge-xl `}
-                onClick={() => setTrigger('Interval')}
-              >
-                Interval
-              </span>
-              <span
-                className={`flex flex-col items-center justify-center p-4 m-4 text-2xl badge ${
-                  trigger == 'Event'
-                    ? 'badge-primary'
-                    : 'badge-outline badge-accent'
-                } badge-xl `}
-                onClick={() => setTrigger('Event')}
-              >
-                Event
-              </span>
+            <div>
+              <h2 className="m-4 text-3xl text-slate-500">Select Trigger</h2>
+              <span className="w-full divider" />
+              <div className="flex flex-row justify-around w-full m-4 overflow-auto">
+                <span
+                  className={`flex flex-col items-center justify-center p-4 m-4 text-2xl badge ${
+                    trigger == 'Time'
+                      ? 'badge-primary'
+                      : 'badge-outline badge-accent'
+                  } badge-xl `}
+                  onClick={() => setTrigger('Time')}
+                >
+                  Time
+                </span>
+                <span
+                  className={`flex flex-col items-center justify-center p-4 m-4 text-2xl badge ${
+                    trigger == 'Interval'
+                      ? 'badge-primary'
+                      : 'badge-outline badge-accent'
+                  } badge-xl `}
+                  onClick={() => setTrigger('Interval')}
+                >
+                  Interval
+                </span>
+                <span
+                  className={`flex flex-col items-center justify-center p-4 m-4 text-2xl badge ${
+                    trigger == 'Event'
+                      ? 'badge-primary'
+                      : 'badge-outline badge-accent'
+                  } badge-xl `}
+                  onClick={() => setTrigger('Event')}
+                >
+                  Event
+                </span>
+              </div>
+
+              {trigger == 'Time' ? (
+                <div className="flex flex-col items-start justify-center w-full p-4 m-4 text-2xl shadow-lg bg-base-100 rounded-xl">
+                  <h1 className="m-4 text-2xl font-medium text-slate-500">
+                    Choose the Time
+                  </h1>
+                  <input
+                    className="w-full text-2xl border-2 input input-primary input-bordered"
+                    type="datetime-local"
+                    onChange={(e) =>
+                      setTime(new Date(e.target.value).getTime())
+                    }
+                    required
+                  />
+                </div>
+              ) : (
+                trigger && (
+                  <div className="flex flex-wrap items-center justify-around w-full p-4 m-4 text-2xl shadow-lg bg-base-100 rounded-xl">
+                    {sensors &&
+                      trigger &&
+                      trigger != 'Time' &&
+                      sensors.map((sensor) => (
+                        <span
+                          key={sensor.id}
+                          className={`flex flex-col items-center justify-center p-4 m-4 text-2xl badge ${
+                            selectSensor == sensor.id
+                              ? 'badge-primary'
+                              : 'badge-outline badge-accent'
+                          } badge-xl `}
+                          onClick={() => setSelectSensor(sensor.id)}
+                        >
+                          {sensor.name}
+                        </span>
+                      ))}
+                    {trigger == 'Interval' ? (
+                      <div className="flex flex-row items-center justify-center w-full p-4 m-4 text-2xl shadow-lg bg-base-100 rounded-xl">
+                        <input
+                          className="w-1/3 text-2xl border-2 input input-primary input-bordered "
+                          type="number"
+                          placeholder="Min Value"
+                          onChange={(e) => setSensorRange([e.target.value])}
+                          required
+                        />
+                        <h1 className="m-4 text-3xl font-bold">-</h1>
+                        <input
+                          className="w-1/3 text-2xl border-2 input input-primary input-bordered "
+                          type="number"
+                          placeholder="Max Value"
+                          onChange={(e) => setSensorRange([...sensorRange, e.target.value])}
+                          required
+                        />
+                      </div>
+                    ) : (
+                      <div className="flex flex-row items-center justify-center w-full p-4 m-4 text-2xl bg-base-100 rounded-xl">
+                        <input
+                          className="text-2xl border-2 input input-primary input-bordered "
+                          type="number"
+                          placeholder="Value"
+                          onChange={(e) => setSensorValue(e.target.value)}
+                          required
+                        />
+                      </div>
+                    )}
+                  </div>
+                )
+              )}
             </div>
-
-            {(() => {
-              switch (trigger) {
-                case 'Time':
-                  return (
-                    <div className="flex flex-col items-center justify-center w-full p-4 m-4 text-2xl shadow-lg bg-base-100 rounded-xl">
-                      <input
-                        className="w-full text-2xl border-2 input input-primary input-bordered"
-                        type="text"
-                        placeholder="Enter a time"
-                      />
-                    </div>
-                  );
-
-                case 'Interval':
-                  return (
-                    <div className="flex flex-col items-center justify-center w-full p-4 m-4 text-2xl shadow-lg bg-base-100 rounded-xl">
-                      <input
-                        className="w-full text-2xl border-2 input input-primary input-bordered"
-                        type="text"
-                        placeholder="Enter an interval"
-                      />
-                    </div>
-                  );
-
-                case 'Event':
-                  return (
-                    <div className="flex flex-col items-center justify-center w-full p-4 m-4 text-2xl shadow-lg bg-base-100 rounded-xl">
-                      <input
-                        className="w-full text-2xl border-2 input input-primary input-bordered"
-                        type="text"
-                        placeholder="Enter an event"
-                      />
-                    </div>
-                  );
-
-                default:
-                  return null;
-              }
-            })()}
-
+            <div className="flex flex-col justify-between">
+              <h2 className="m-4 text-3xl text-slate-500">
+                Select a name for the routine
+              </h2>
+              <div className="flex flex-row justify-between my-4 mb-8">
+                <input
+                  className="w-full mx-4 text-2xl border-2 input input-primary input-bordered"
+                  type="text"
+                  placeholder="Enter a name for the routine"
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+                <button
+                  type="submit"
+                  className="text-2xl btn btn-primary"
+                  onClick={(e) => handleSubmit(e)}
+                >
+                  Add Routine
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
