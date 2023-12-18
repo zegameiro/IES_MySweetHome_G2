b719b11393073a6560d41a67083ec2b26d8be773
d479 <daniel.madureira@ua.pt>
2023-12-18 22:15:51 +0000

    Small UI fixes

--- proj/MSH-frontend/src/components/layout/Alert.jsx
+++ proj/MSH-frontend/src/components/layout/Alert.jsx
@@ -1,87 +1,108 @@
-import React, { useEffect, useState } from "react";
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react';
 
-import { BASE_API_URL } from "../../constants";
+import { BASE_API_URL } from '../../constants';
 
-import { IoIosInformationCircleOutline } from "react-icons/io";
-import { IoWarningOutline, IoCloseCircleOutline } from "react-icons/io5";
-import { MdErrorOutline } from "react-icons/md";
-import { FaRegClock } from "react-icons/fa";
+import { IoIosInformationCircleOutline } from 'react-icons/io';
+import { IoWarningOutline, IoCloseCircleOutline } from 'react-icons/io5';
+import { MdErrorOutline } from 'react-icons/md';
+import { FaRegClock } from 'react-icons/fa';
 
-import axios from "axios";
-
+import axios from 'axios';
 
 const Alert = ({ alert, removeAl }) => {
+  const [isHiding, setIsHiding] = useState(false);
+  const [isInitial, setIsInitial] = useState(true);
 
-    const [isHiding, setIsHiding] = useState(false);
-    const [isInitial, setIsInitial] = useState(true);
+  const convertTimestamp = (timestamp) => {
+    timestamp = timestamp * 1000;
+    const date = new Date(timestamp);
 
-    const convertTimestamp = (timestamp) => {
-      timestamp = timestamp * 1000
-      const date = new Date(timestamp);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    return `${hours}h ${minutes}min`;
+  };
 
-      const hours = date.getHours();
-      const minutes = date.getMinutes();
-      return `${hours}h ${minutes}min`;
-    };
+  const backgroundColor =
+    alert.alert_level === 3
+      ? 'alert-info'
+      : alert.alert_level === 2
+      ? 'alert-warning'
+      : 'alert-error';
 
-    const backgroundColor =
-        alert.alert_level === 3
-        ? "alert-info"
-        : alert.alert_level === 2
-        ? "alert-warning"
-        : "alert-error";
-    
-    const icon = 
-        alert.alert_level === 3
-        ? <IoIosInformationCircleOutline />
-        : alert.alert_level === 2
-        ? <IoWarningOutline />
-        : <MdErrorOutline />;
+  const icon =
+    alert.alert_level === 3 ? (
+      <IoIosInformationCircleOutline />
+    ) : alert.alert_level === 2 ? (
+      <IoWarningOutline />
+    ) : (
+      <MdErrorOutline />
+    );
 
-    const removeAlert = async (alertId) => {
-        setIsHiding(true);
-        setTimeout(async () => {
-            try {
-              const res = await axios.post(`${BASE_API_URL}/alerts/mark?id=${alertId}`);
-              removeAl(alertId);
-            } catch (error) {
-              console.log(error);
-            }
-        }, 500);
+  const removeAlert = async (alertId) => {
+    setIsHiding(true);
+    setTimeout(async () => {
+      try {
+        const res = await axios.post(
+          `${BASE_API_URL}/alerts/mark?id=${alertId}`
+        );
+        removeAl(alertId);
+      } catch (error) {
+        console.log(error);
+      }
+    }, 500);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeAlert(alert.id);
+    }, 15000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (isInitial) {
+      setTimeout(() => {
+        setIsInitial(false);
+      }, 50);
     }
+  }, [isInitial]);
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-          removeAlert(alert.id);
-        }, 15000);
-    
-        return () => clearTimeout(timer);
-    }, []);
-
-    useEffect(() => {
-        if (isInitial) {
-          setTimeout(() => {
-            setIsInitial(false);
-          }, 50);
-        }
-      }, [isInitial]);
-    
-    return (
-      <div className="pt-5">
-        <div role="alert" className={`alert ${backgroundColor} shadow-lg text-white transform transition-all ease-in-out duration-500 ${isHiding ? 'opacity-0 scale-0' : (isInitial ? 'opacity-0 scale-0' : 'opacity-100 scale-100')}`}>
-            <div className="text-3xl font-bold">{icon}</div>
-            <div>
-                <div className="flex flex-row font-bold">
-                    <h3 className="text-lg">{alert.alert_header}</h3>
-                    <span className="flex flex-row text-lg"><span className=" pl-5 pt-[5px]"><FaRegClock/> </span>{convertTimestamp(alert.timestamp)}</span>
-                </div>
-                <div className="text-md">{alert.alert_information}</div>
-            </div>
-            <button onClick={() => removeAlert(alert.id)}> <span className="text-3xl font-bold"><IoCloseCircleOutline /></span></button>
+  return (
+    <div className="w-full pt-5">
+      <div
+        role="alert"
+        className={`alert ${backgroundColor} shadow-lg text-white transform transition-all ease-in-out duration-500 ${
+          isHiding
+            ? 'opacity-0 scale-0'
+            : isInitial
+            ? 'opacity-0 scale-0'
+            : 'opacity-100 scale-100'
+        }`}
+      >
+        <div className="text-3xl font-bold">{icon}</div>
+        <div>
+          <div className="flex flex-row font-bold">
+            <h3 className="text-lg">{alert.alert_header}</h3>
+            <span className="flex flex-row text-lg">
+              <span className=" pl-5 pt-[5px]">
+                <FaRegClock />{' '}
+              </span>
+              {convertTimestamp(alert.timestamp)}
+            </span>
+          </div>
+          <div className="text-md">{alert.alert_information}</div>
         </div>
+        <button onClick={() => removeAlert(alert.id)}>
+          {' '}
+          <span className="text-3xl font-bold">
+            <IoCloseCircleOutline />
+          </span>
+        </button>
       </div>
-    )
-    
+    </div>
+  );
 };
 
 export default Alert;
--- proj/MSH-frontend/src/components/pages/Dashboard.jsx
+++ proj/MSH-frontend/src/components/pages/Dashboard.jsx
@@ -1,26 +1,26 @@
-import { useEffect, useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import { BASE_API_URL } from "../../constants";
+import { useEffect, useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { BASE_API_URL } from '../../constants';
 
-import { IoIosCloudy, IoIosArrowDown } from "react-icons/io";
-import { FaTemperatureHalf } from "react-icons/fa6";
+import { IoIosArrowDown } from 'react-icons/io';
 
-import OutputDeviceCard from "../layout/OutputDeviceCard";
-import InputDeviceCard from "../layout/InputDeviceCard";
-import Header from "../layout/Header";
-import Navbar from "../layout/Navbar";
-import Alert from "../layout/Alert";
+import OutputDeviceCard from '../layout/OutputDeviceCard';
+import InputDeviceCard from '../layout/InputDeviceCard';
+import Header from '../layout/Header';
+import Navbar from '../layout/Navbar';
+import Alert from '../layout/Alert';
 
-import axios from "axios";
+import axios from 'axios';
+import Carroussel from '../layout/Carroussel';
 
 const Dashboard = () => {
   const navigate = useNavigate();
 
-  const user = JSON.parse(localStorage.getItem("user"))
-    ? JSON.parse(localStorage.getItem("user"))
+  const user = JSON.parse(localStorage.getItem('user'))
+    ? JSON.parse(localStorage.getItem('user'))
     : null;
 
-  const [selectedRoom, setSelectedRoom] = useState("");
+  const [selectedRoom, setSelectedRoom] = useState('');
   const [filteredOutDevices, setFilteredOutDevices] = useState([]);
   const [outputDevices, setOutputDevices] = useState([]);
   const [inputDevices, setInputDevices] = useState([]);
@@ -70,35 +70,35 @@ const Dashboard = () => {
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const removeAlert = (alertId) => {
-    setAlerts(alerts.filter(alert => alert.id !== alertId));
-  }
+    setAlerts(alerts.filter((alert) => alert.id !== alertId));
+  };
 
   useEffect(() => {
-    if (localStorage.getItem("user")) {
+    if (localStorage.getItem('user')) {
       getOutputDevices();
       getInputDevices();
       getAllRooms();
       getAlerts();
     } else {
-      navigate("/login?redirect=dashboard");
+      navigate('/login?redirect=dashboard');
     }
   }, []);
 
   useEffect(() => {
-  const interval = setInterval(() => {
-    getAlerts();
-  }, 5000); // Fetch new alerts every 5 seconds
+    const interval = setInterval(() => {
+      getAlerts();
+    }, 1); // Fetch new alerts every 5 seconds
 
-  return () => clearInterval(interval); // Clean up on unmount
-}, []);
+    return () => clearInterval(interval); // Clean up on unmount
+  }, []);
 
   useEffect(() => {
     if (selectedRoom) {
-      const roomOutDevices = outputDevices.filter(
-        (outdevice) => selectedRoom.devices.includes(outdevice.id)
+      const roomOutDevices = outputDevices.filter((outdevice) =>
+        selectedRoom.devices.includes(outdevice.id)
       );
       setFilteredOutDevices(roomOutDevices);
     } else {
@@ -112,194 +112,108 @@ const Dashboard = () => {
         <Navbar />
         <div className="flex flex-col w-full h-full">
           <Header />
-          <div className="flex flex-col pl-[5%] pt-[2%] w-[100%]">
-            <div className="carousel w-[70%] rounded-3xl">
-              <div id="slide1" className="carousel-item relative w-[100%]">
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                  <a
-                    href="#slide3"
-                    className="btn btn-outline btn-circle btn-secondary"
-                  >
-                    ❮
-                  </a>
-                  <a
-                    href="#slide2"
-                    className="btn btn-outline btn-circle btn-secondary"
-                  >
-                    ❯
-                  </a>
-                </div>
-                <div
-                  className={`hero rounded-3xl w-[100%] h-[100%] bg-[url('/src/assets/images/home1.jpg')]`}
-                >
-                  <div className="hero-overlay rounded-3xl w-[100%] h-[100%] bg-[#101010] bg-opacity-50 "></div>
-                  <div className="text-center text-white hero-content">
-                    <div className="max-w-xl">
-                      <h2 className="mb-5 text-5xl font-bold">
-                        Welcome Back {user.firstname}!
-                      </h2>
-                      <p className="mb-5 text-2xl">
-                        Welcome home! We hope you're having a great day.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div id="slide2" className="carousel-item relative w-[100%]">
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                  <a
-                    href="#slide1"
-                    className="btn btn-outline btn-circle btn-secondary"
-                  >
-                    ❮
-                  </a>
-                  <a
-                    href="#slide3"
-                    className="btn btn-outline btn-circle btn-secondary"
-                  >
-                    ❯
-                  </a>
-                </div>
-                <div
-                  className={`hero rounded-3xl bg-[url('/src/assets/images/home2.jpg')]`}
-                >
-                  <div className="hero-overlay rounded-3xl bg-[#101010] bg-opacity-50 "></div>
-                  <div className="text-center text-white hero-content">
-                    <div className="max-w-xl">
-                      <h1 className="mb-5 text-5xl font-bold">
-                        Today's Weather
-                      </h1>
-                      <div className="flex flex-row items-center">
-                        <FaTemperatureHalf className="text-5xl" />{" "}
-                        <p className="text-2xl">
-                          Enjoy a comfortable home at a cozy 25°C.
-                        </p>
-                      </div>
-                      <div className="flex flex-row items-center">
-                        <IoIosCloudy className="text-5xl" />{" "}
-                        <p className="text-2xl">It's a cloudy day today.</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div id="slide3" className="carousel-item relative w-[100%]">
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                  <a
-                    href="#slide2"
-                    className="btn btn-outline btn-circle btn-secondary"
-                  >
-                    ❮
-                  </a>
-                  <a
-                    href="#slide1"
-                    className="btn btn-outline btn-circle btn-secondary"
-                  >
-                    ❯
-                  </a>
-                </div>
-                <div
-                  className={`hero rounded-3xl bg-[url('/src/assets/images/home3.jpg')]`}
-                >
-                  <div className="hero-overlay rounded-3xl bg-[#101010] bg-opacity-50 "></div>
-                  <div className="text-center text-white hero-content">
-                    <div className="max-w-xl">
-                      <h1 className="mb-5 text-5xl font-bold">
-                        Missing Something
-                      </h1>
-                      <p className="mb-5 text-2xl">
-                        Help us to know what it is, create you're own routines
-                        that make you feel more confortable at you're home
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            
-            <div className="pt-5 flex flex-row text-center items-center space-x-[15%]">
-              <h1 className="text-6xl font-bold">Devices</h1>
-              <div className="dropdown">
-                <div
-                  tabIndex={0}
-                  role="button"
-                  className="m-1 text-xl text-white btn btn-primary"
-                >
-                  {selectedRoom ? selectedRoom.name : "Select Room"}{" "}
-                  <IoIosArrowDown />
-                </div>
-                <ul className="dropdown-content z-[10] menu p-2 shadow bg-base-100 rounded-box border primary border-[4px] border-primary w-52 text-xl">
-                  <li
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      setSelectedRoom("");
-                    }}
-                  >
-                    <button>Select Room</button>
-                  </li>
-                  {rooms.map((room, idx) => (
-                    <li
-                      key={idx}
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        setSelectedRoom(room);
-                      }}
+          <div className="flex flex-row w-full mx-4">
+            <div className="flex flex-col w-2/3 p-4">
+              <Carroussel />
+              <div className="flex flex-col w-full m-4">
+                <div className="flex flex-row items-center justify-between w-full p-4 text-center">
+                  <h1 className="text-6xl font-bold">Devices</h1>
+                  <div className="dropdown">
+                    <div
+                      tabIndex={0}
+                      role="button"
+                      className="m-1 text-xl text-white btn btn-primary"
                     >
-                      <button>{room.name}</button>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <Link to="/devices">
-                <p className="font-semibold text-neutral">See more</p>
-              </Link>
-            </div>
-            <div className="divider w-[70%] text-xl font-semibold pt-[4%] pb-[1%]">
-              {" "}
-              Output Devices{" "}
-            </div>
-            <div className="flex flex-row max-w-[85vw]">
-              {selectedRoom !== "" ? (
-                filteredOutDevices.length > 0 ? (
-                  <div className="flex flex-row max-w-[70%] space-x-4 overflow-x-auto overflow-y-hidden custom-scrollbar">
-                    {filteredOutDevices.map((device) => (
-                      <OutputDeviceCard
-                        key={device.id}
-                        device={device}
-                        room={selectedRoom}
-                      />
-                    ))}
+                      {selectedRoom ? selectedRoom.name : 'Select Room'}{' '}
+                      <IoIosArrowDown />
+                    </div>
+                    <ul className="dropdown-content z-[10] menu p-2 shadow bg-base-100 rounded-box border-2 primary border-primary w-52 text-xl">
+                      <li
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          setSelectedRoom(null);
+                        }}
+                      >
+                        <button>Select Room</button>
+                      </li>
+                      {rooms.map((room, idx) => (
+                        <li
+                          key={idx}
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            setSelectedRoom(room);
+                          }}
+                        >
+                          <button>{room.name}</button>
+                        </li>
+                      ))}
+                    </ul>
                   </div>
-                ) : (
-                  <h2 className="text-xl">No devices found</h2>
-                )
-              ) : (
-                <h2 className="text-xl">No devices found</h2>
-              )}
-            </div>
-            <div className="divider w-[70%] text-xl font-semibold pt-[4%] pb-[1%]">
-              {" "}
-              Input Devices{" "}
-            </div>
-            <div className="flex flex-row max-w-[85vw]">
-              {inputDevices.length > 0 ? (
-                <div className="flex flex-row max-w-[70%] space-x-4 overflow-x-auto overflow-y-hidden custom-scrollbar">
-                  {inputDevices.map((device) => (
-                    <InputDeviceCard key={device.id} device={device} />
-                  ))}
                 </div>
-              ) : (
-                <h2 className="text-xl">No devices found</h2>
-              )}
+
+                <div className="w-full text-xl font-semibold divider ">
+                  Output Devices
+                </div>
+                <div className="flex flex-row max-w-[85vw]">
+                  {selectedRoom !== '' ? (
+                    filteredOutDevices.length > 0 ? (
+                      <div className="flex flex-row max-w-[70%] space-x-4 overflow-x-auto overflow-y-hidden custom-scrollbar">
+                        {filteredOutDevices.map((device) => (
+                          <OutputDeviceCard
+                            key={device.id}
+                            device={device}
+                            room={selectedRoom}
+                          />
+                        ))}
+                      </div>
+                    ) : (
+                      <span className="flex flex-row justify-center w-full text-xl">
+                        <h2 className="m-8 text-xl">No devices found</h2>
+                      </span>
+                    )
+                  ) : (
+                    <h2 className="m-8 text-xl">No devices found</h2>
+                  )}
+                </div>
+                <div className="w-full text-xl font-semibold divider">
+                  Input Devices
+                </div>
+                <div className="flex flex-row w-full">
+                  {inputDevices.length > 0 ? (
+                    <div className="flex flex-row w-full space-x-4 overflow-x-auto overflow-y-hidden custom-scrollbar">
+                      {inputDevices.map((device) => (
+                        <InputDeviceCard
+                          key={device.id}
+                          device={device}
+                        />
+                      ))}
+                    </div>
+                  ) : (
+                    <h2 className="text-xl">No devices found</h2>
+                  )}
+                </div>
+              </div>
             </div>
-        <div className="absolute flex flex-col bottom-2 right-2">
-              {alerts.length > 0 ? 
+            <div className=" absolute right-[2rem] flex flex-col items-start w-[30%] p-4 rounded-2xl">
+              <h1 className="p-2 text-4xl font-bold">Alerts</h1>
+              <hr />
+              {alerts.length > 0 ? (
                 alerts.map((alert) => (
-                  <Alert key={alert.id} alert={alert} removeAl={removeAlert}/>
+                  <span key={alert.id}
+                    
+                  >
+                    
+                  <Alert
+                    alert={alert}
+                    removeAl={removeAlert}
+                  />
+                  </span>
                 ))
-              : 
-                null
-              }
+              ) : (
+                <span className='flex justify-center p-8'>
+                  <p className="text-xl">No alerts found</p>
+                </span>
+              )}
             </div>
           </div>
         </div>
